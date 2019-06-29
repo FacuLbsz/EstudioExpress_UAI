4c89b2db14d24f0e42eb7f9a416d99d82dd8ba23
FacuLbsz <facuvl.94@gmail.com>
2019-06-29 12:29:59 -0300

    Se crean Pages de cursos y pago

---
+++ EstudioExpress/Pages/Curso.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Curso.aspx.cs" Inherits="EstudioExpress.Pages.Curso" MasterPageFile="~/Site.Master" %>
+
+<asp:Content ID="Content" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container custom-container">
+    </div>
+</asp:Content>
---
+++ EstudioExpress/Pages/Pago.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace EstudioExpress.Pages {
+    
+    
+    public partial class Pago {
+    }
+}
